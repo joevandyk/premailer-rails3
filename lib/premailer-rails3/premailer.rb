553c50d8fce58d6777fedf82bdc28afba985133f
Joe Van Dyk <joe@fixieconsulting.com>
2011-11-18 11:42:18 -0800

    Don't do css processing in premailer-rails3 anymore

--- lib/premailer-rails3/premailer.rb
+++ lib/premailer-rails3/premailer.rb
@@ -1,26 +1,12 @@
 module PremailerRails
   class Premailer < ::Premailer
     def initialize(html)
-      # In order to pass the CSS as string to super it is necessary to access
-      # the parsed HTML beforehand. To do so, the adapter needs to be
-      # initialized. The ::Premailer::Adaptor handles the discovery of a
-      # suitable adaptor (Nokogiri or Hpricot). To make load_html work, an
-      # adaptor needs to be included and @options[:with_html_string] needs to be
-      # set. For further information, refer to ::Premailer#initialize.
-      @options = { :with_html_string => true }
-      ::Premailer.send(:include, Adapter.find(Adapter.use))
-      doc = load_html(html)
-
-      css_string = CSSHelper.css_for_doc(doc)
-      doc = CSSHelper.remove_links(doc)
-
       options = {
         :with_html_string => true,
         :preserve_styles => true,
-        :include_style_tags => false,
-        :css_string       => css_string
+        :include_style_tags => false
       }
-      super(doc.to_html, options)
+      super(html, options)
     end
   end
 end
